466838e701e06fb581359565bdb485bb13501a48
monattv <stefanogiaccone.email@gmail.com>
2026-07-28 19:10:36 +0200

    Update prodotti.js

--- admin/prodotti.js
+++ admin/prodotti.js
@@ -2,26 +2,11 @@ const API_URL =
 "https://script.google.com/macros/s/AKfycbyX9Kvz-Aa0PqfIoFLShhRcSuQYiffFFpZJKRncx_3S94PDN7-o83LGTHO5QrxILQ2V/exec";
 
 
+document.addEventListener("DOMContentLoaded", ()=>{
 
-let prodottiGlobali = [];
+    caricaProdotti();
 
-
-
-
-
-document.addEventListener(
-"DOMContentLoaded",
-()=>{
-
-caricaProdotti();
-
-}
-
-);
-
-
-
-
+});
 
 
 
@@ -33,20 +18,52 @@ document.getElementById("prodotti");
 
 
 area.innerHTML =
-"⏳ Caricamento prodotti...";
+"⏳ Avvio caricamento...";
+
+
+
+console.log("INIZIO FETCH");
 
 
 
 fetch(API_URL)
 
 
-.then(r=>r.json())
+.then(response=>{
 
 
-.then(prodotti=>{
+console.log("RISPOSTA RICEVUTA", response);
 
 
-prodottiGlobali = prodotti;
+return response.text();
+
+
+})
+
+
+.then(testo=>{
+
+
+console.log("TESTO SERVER:", testo);
+
+
+
+let prodotti;
+
+
+try{
+
+prodotti = JSON.parse(testo);
+
+}
+
+catch(e){
+
+throw new Error(
+"Il server non ha restituito JSON"
+);
+
+}
 
 
 
@@ -57,220 +74,52 @@ area.innerHTML="";
 prodotti.forEach((p,index)=>{
 
 
-
 area.innerHTML += `
 
-
 <div class="ordine-box">
-
 
 <h3>
 ${p.Nome}
 </h3>
 
-
-
 <p>
-💰 Prezzo: ${p.Prezzo} €
+Prezzo: ${p.Prezzo} €
 </p>
-
 
 <p>
 Disponibile: ${p.Disponibile}
 </p>
 
-
-<p>
-Colori: ${p.Colori}
-</p>
-
-
-<p>
-Forme: ${p.Forme}
-</p>
-
-
-
 <button onclick="modificaProdotto(${index})">
-
 ✏️ Modifica
-
 </button>
-
 
 
 </div>
 
-
-
 `;
 
 
-
 });
-
 
 
 })
 
 
-.catch(e=>{
+.catch(error=>{
 
 
-console.error(e);
+console.error(
+"ERRORE:",
+error
+);
 
 
 area.innerHTML =
-"❌ Errore caricamento prodotti";
+"❌ Errore: "+error.message;
 
 
 });
-
-
-}
-
-
-
-
-
-
-
-
-function modificaProdotto(index){
-
-
-const p =
-prodottiGlobali[index];
-
-
-
-const area =
-document.getElementById("prodotti");
-
-
-
-area.innerHTML = `
-
-
-
-<div class="ordine-box">
-
-
-<h2>
-Modifica ${p.Nome}
-</h2>
-
-
-
-<label>
-Nome
-</label>
-
-
-<input id="nomeMod"
-value="${p.Nome}">
-
-
-
-<label>
-Prezzo
-</label>
-
-
-<input id="prezzoMod"
-value="${p.Prezzo}">
-
-
-
-<label>
-Disponibilità
-</label>
-
-
-<select id="dispMod">
-
-
-<option ${p.Disponibile=="SI"?"selected":""}>
-SI
-</option>
-
-
-<option ${p.Disponibile=="NO"?"selected":""}>
-NO
-</option>
-
-
-</select>
-
-
-
-
-
-<label>
-Colori
-</label>
-
-
-<input id="coloriMod"
-value="${p.Colori}">
-
-
-
-
-
-<label>
-Forme
-</label>
-
-
-<input id="formeMod"
-value="${p.Forme}">
-
-
-
-
-
-<button onclick="salvaModifica(${p.ID})">
-
-💾 Salva
-
-</button>
-
-
-
-
-<button onclick="caricaProdotti()">
-
-❌ Annulla
-
-</button>
-
-
-
-</div>
-
-
-`;
-
-
-
-}
-
-
-
-
-
-
-
-
-
-function salvaModifica(id){
-
-
-alert(
-"Salvataggio prodotto ID "+id+" pronto"
-);
-
 
 
 }
