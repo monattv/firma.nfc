f12cb9c63981361581c7d7149451457d47290912
monattv <stefanogiaccone.email@gmail.com>
2026-07-28 14:41:27 +0200

    Update carrello.js

--- js/carrello.js
+++ js/carrello.js
@@ -1,39 +1,18 @@
-const container = document.getElementById(
+const container =
+document.getElementById(
 "carrello-container"
 );
 
 
 
-let prodotto =
+let carrello =
 JSON.parse(
-localStorage.getItem("prodottoCarrello")
-);
+localStorage.getItem("carrello")
+) || [];
 
-
-
-
-
-if(!prodotto){
-
-
-container.innerHTML = `
-
-<h3>
-Il carrello è vuoto
-</h3>
-
-`;
-
-
-
-}
-else{
 
 
 mostraCarrello();
-
-
-}
 
 
 
@@ -44,87 +23,116 @@ mostraCarrello();
 function mostraCarrello(){
 
 
+if(carrello.length===0){
+
 
 container.innerHTML = `
+
+<h3>
+Carrello vuoto
+</h3>
+
+`;
+
+return;
+
+}
+
+
+
+container.innerHTML="";
+
+
+
+let totale=0;
+
+
+
+
+carrello.forEach((prodotto,index)=>{
+
+
+
+let prezzo =
+Number(prodotto.prezzo);
+
+
+
+totale += prezzo * prodotto.quantita;
+
+
+
+container.innerHTML += `
 
 
 <div class="cart-card">
 
 
 <h2>
-
 ${prodotto.nome}
-
 </h2>
 
 
-
 <p>
-
 Colore:
-
-${prodotto.colore || "Non scelto"}
-
+${prodotto.colore || "-"}
 </p>
 
 
-
-
 <p>
-
 Forma:
-
-${prodotto.forma || "Non scelta"}
-
+${prodotto.forma || "-"}
 </p>
 
 
-
-
-
 <p>
-
-Incisione:
-
-${prodotto.incisione || "Nessuna"}
-
-</p>
-
-
-
-
-
-<p>
-
 Testo:
-
 ${prodotto.testo || "-"}
-
 </p>
 
+
+<p>
+Anello:
+${prodotto.anello || "-"}
+</p>
 
 
 
 <p>
 
-Anello:
+Quantità:
 
-${prodotto.anello || "Non scelto"}
+<button onclick="meno(${index})">
+-
+</button>
+
+
+${prodotto.quantita}
+
+
+<button onclick="piu(${index})">
++
+</button>
+
 
 </p>
-
 
 
 
 
 <h3>
 
-Prezzo:
-
-${prodotto.prezzo} €
+${prezzo.toFixed(2)} €
 
 </h3>
 
+
+
+<button onclick="rimuovi(${index})">
+
+Elimina
+
+</button>
 
 
 </div>
@@ -135,10 +143,100 @@ ${prodotto.prezzo} €
 
 
 
+});
+
+
+
+
+
+container.innerHTML += `
+
+
+<h2>
+
+Totale:
+${totale.toFixed(2)} €
+
+</h2>
+
+
+`;
+
+
+
 }
 
 
 
+
+
+
+
+function piu(index){
+
+
+carrello[index].quantita++;
+
+
+salva();
+
+
+}
+
+
+
+
+
+function meno(index){
+
+
+if(carrello[index].quantita>1){
+
+carrello[index].quantita--;
+
+}
+
+
+salva();
+
+
+}
+
+
+
+
+
+function rimuovi(index){
+
+
+carrello.splice(index,1);
+
+
+salva();
+
+
+}
+
+
+
+
+
+function salva(){
+
+
+localStorage.setItem(
+
+"carrello",
+
+JSON.stringify(carrello)
+
+);
+
+
+mostraCarrello();
+
+
+}
 
 
 
