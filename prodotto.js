1d9c3dbebd54b3c796149bc6b4de30e57fb7b9d0
monattv <stefanogiaccone.email@gmail.com>
2026-07-28 13:57:15 +0200

    Update prodotto.js

--- prodotto.js
+++ prodotto.js
@@ -1,1 +1,227 @@
+const API_URL = "METTI IL TUO LINK APPS SCRIPT";
 
+
+
+const params = new URLSearchParams(
+    window.location.search
+);
+
+
+const idProdotto = params.get("id");
+
+
+
+const nome = document.getElementById(
+    "nome-prodotto"
+);
+
+
+
+const area = document.getElementById(
+    "personalizzazione"
+);
+
+
+
+
+
+fetch(API_URL)
+
+.then(response => response.json())
+
+.then(prodotti => {
+
+
+    const prodotto = prodotti.find(
+        p => p.ID == idProdotto
+    );
+
+
+
+    if(!prodotto){
+
+        nome.innerHTML =
+        "Prodotto non trovato";
+
+        return;
+
+    }
+
+
+
+    mostraProdotto(prodotto);
+
+
+});
+
+
+
+
+
+
+function mostraProdotto(prodotto){
+
+
+
+    nome.innerHTML = prodotto.Nome;
+
+
+
+    area.innerHTML = `
+
+
+<h3>
+Personalizza il tuo prodotto
+</h3>
+
+
+
+<h4>
+Colore
+</h4>
+
+
+<div>
+
+${creaBottoni(
+    prodotto.Colori
+)}
+
+</div>
+
+
+
+<h4>
+Forma
+</h4>
+
+
+<div>
+
+${creaBottoni(
+    prodotto.Forme
+)}
+
+</div>
+
+
+
+
+<h4>
+Incisione
+</h4>
+
+
+<div>
+
+
+<label>
+
+<input type="radio" name="incisione">
+
+Testo
+
+</label>
+
+
+<label>
+
+<input type="radio" name="incisione">
+
+Immagine
+
+</label>
+
+
+</div>
+
+
+
+
+<h4>
+Testo incisione
+</h4>
+
+
+<input 
+type="text"
+placeholder="Scrivi qui">
+
+
+
+
+
+<h4>
+Carica immagine
+</h4>
+
+
+<input 
+type="file">
+
+
+
+
+
+<h4>
+Colore anello
+</h4>
+
+
+<div>
+
+${creaBottoni(
+    prodotto.ColoreAnello
+)}
+
+</div>
+
+
+<button>
+
+Aggiungi al carrello
+
+</button>
+
+
+`;
+
+
+
+}
+
+
+
+
+
+
+
+function creaBottoni(lista){
+
+
+if(!lista){
+
+return "";
+
+}
+
+
+return lista
+.split(",")
+.map(elemento => {
+
+
+return `
+
+<button class="option">
+
+${elemento}
+
+</button>
+
+`;
+
+})
+.join("");
+
+}
