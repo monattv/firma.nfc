60c67e3d024aa6d288a4607c6e8bff778680e182
monattv <stefanogiaccone.email@gmail.com>
2026-07-28 13:13:14 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbyX9Kvz-Aa0PqfIoFLShhRcSuQYiffFFpZJKRncx_3S94PDN7-o83LGTHO5QrxILQ2V/exec";
 
+
+fetch(API_URL)
+
+.then(risposta => risposta.json())
+
+.then(prodotti => {
+
+console.log(prodotti);
+
+
+});
