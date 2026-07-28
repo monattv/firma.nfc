699e071b9caf0ab3092e36b963995093fd614599
monattv <stefanogiaccone.email@gmail.com>
2026-07-28 19:06:35 +0200

    Update prodotti.js

--- admin/prodotti.js
+++ admin/prodotti.js
@@ -3,6 +3,27 @@ const API_URL =
 
 
 
+let prodottiGlobali = [];
+
+
+
+
+
+document.addEventListener(
+"DOMContentLoaded",
+()=>{
+
+caricaProdotti();
+
+}
+
+);
+
+
+
+
+
+
 
 function caricaProdotti(){
 
@@ -11,10 +32,8 @@ const area =
 document.getElementById("prodotti");
 
 
-
 area.innerHTML =
 "⏳ Caricamento prodotti...";
-
 
 
 
@@ -27,8 +46,7 @@ fetch(API_URL)
 .then(prodotti=>{
 
 
-
-console.log(prodotti);
+prodottiGlobali = prodotti;
 
 
 
@@ -36,83 +54,46 @@ area.innerHTML="";
 
 
 
-prodotti.forEach(p=>{
+prodotti.forEach((p,index)=>{
 
 
 
 area.innerHTML += `
 
 
-
 <div class="ordine-box">
 
 
-
 <h3>
-
 ${p.Nome}
-
 </h3>
 
 
 
 <p>
-
-💰 Prezzo:
-
-${p.Prezzo} €
-
+💰 Prezzo: ${p.Prezzo} €
 </p>
-
-
 
 
 <p>
-
-Disponibile:
-
-${p.Disponibile}
-
+Disponibile: ${p.Disponibile}
 </p>
-
 
 
 <p>
-
-Colori:
-
-${p.Colori}
-
+Colori: ${p.Colori}
 </p>
-
-
 
 
 <p>
-
-Forme:
-
-${p.Forme}
-
+Forme: ${p.Forme}
 </p>
 
 
 
+<button onclick="modificaProdotto(${index})">
 
-<p>
-
-Incisione:
-
-${p.Incisione}
-
-</p>
-
-
-
-
-<button>
-
-Modifica
+✏️ Modifica
 
 </button>
 
@@ -133,18 +114,162 @@ Modifica
 })
 
 
-.catch(err=>{
+.catch(e=>{
 
 
-console.error(err);
+console.error(e);
 
 
-area.innerHTML=
-
+area.innerHTML =
 "❌ Errore caricamento prodotti";
 
 
 });
+
+
+}
+
+
+
+
+
+
+
+
+function modificaProdotto(index){
+
+
+const p =
+prodottiGlobali[index];
+
+
+
+const area =
+document.getElementById("prodotti");
+
+
+
+area.innerHTML = `
+
+
+
+<div class="ordine-box">
+
+
+<h2>
+Modifica ${p.Nome}
+</h2>
+
+
+
+<label>
+Nome
+</label>
+
+
+<input id="nomeMod"
+value="${p.Nome}">
+
+
+
+<label>
+Prezzo
+</label>
+
+
+<input id="prezzoMod"
+value="${p.Prezzo}">
+
+
+
+<label>
+Disponibilità
+</label>
+
+
+<select id="dispMod">
+
+
+<option ${p.Disponibile=="SI"?"selected":""}>
+SI
+</option>
+
+
+<option ${p.Disponibile=="NO"?"selected":""}>
+NO
+</option>
+
+
+</select>
+
+
+
+
+
+<label>
+Colori
+</label>
+
+
+<input id="coloriMod"
+value="${p.Colori}">
+
+
+
+
+
+<label>
+Forme
+</label>
+
+
+<input id="formeMod"
+value="${p.Forme}">
+
+
+
+
+
+<button onclick="salvaModifica(${p.ID})">
+
+💾 Salva
+
+</button>
+
+
+
+
+<button onclick="caricaProdotti()">
+
+❌ Annulla
+
+</button>
+
+
+
+</div>
+
+
+`;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+function salvaModifica(id){
+
+
+alert(
+"Salvataggio prodotto ID "+id+" pronto"
+);
 
 
 
